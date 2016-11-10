64db11575912674ada33a17ae67415ce413e5865
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-11-10 15:29:01 +0100

    0.1.1: Set up tests, Travis

---
+++ uexpress.spec.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect
+var uexpress = require('./uexpress')
+
+describe('uexpress', function(){
+	it('is a function', function(){
+		expect(uexpress).to.be.a('function')
+	})
+})
